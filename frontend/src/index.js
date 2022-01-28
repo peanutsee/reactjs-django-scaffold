f8747d2e4d4dad9f06166be48be66d3c8784474e
Parcelvy-Admin <dsee007@yahoo.com>
2022-01-28 21:38:22 +0800

    added bootstrap css, fontawesome cdn, manifest.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// Import CSS template file here -> import "./bootstrap.min.css";
+import "./bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
